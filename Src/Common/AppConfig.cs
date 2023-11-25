3ef4e51c60e95910a6e951150764e4655bb563ec
Vlad Dyshakov <kawaii.nekotech@gmail.com>
2023-11-25 09:02:58 +0700

    Added minio, postgres docker compose

---
+++ Src/Common/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicPlayerBackend.Common;
+
+public sealed class AppConfig
+{
+    public Minio Minio { get; set; } = null!;
+}
+
+public sealed class Minio
+{
+    public string? Endpoint { get; set; }
+    public string? AccessKey { get; set; }
+    public string? SecretKey  { get; set; }
+}
